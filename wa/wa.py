5124ec242107e1d844a9525f6c3da94d58f05bbc
shungjs <75958816+shungjs@users.noreply.github.com>
2025-02-26 11:31:03 -0600

    Update wa.py

--- wa/wa.py
+++ wa/wa.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-melbourne_file_path ='C:/Users/isu_scnayah/Downloads/wa/melb_data.csv'
+melbourne_file_path ='melb_data.csv'
 
 melbourne_data =pd.read_csv(melbourne_file_path)
 
